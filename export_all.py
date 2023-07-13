529d6ff4f11e1f3d6bb4893d3c0d2973188fab01
Zai Dium <zai.dium@yahoo.com>
2023-07-13 19:35:49 +0300

    Improve export objects to some objects and other on one file

--- export_all.py
+++ export_all.py
@@ -4,7 +4,8 @@ from bl_ui.utils import PresetPanel
 from bpy.types import Panel, Menu
 
 # Replace these with the names of the objects you want to export
-#object_names = ["HullConvex", "WheelsConvex", "WheelConvex"]
+object_names = ["HullConvex", "WheelsConvex", "WheelConvex", "WingsConvex",
+                "Hull", "Wheels", "Wheel", "Wings"]
 #export_preset_name = "My"
 
 # Replace this with the path to the folder where you want to export the DAE files
@@ -17,12 +18,16 @@ if not os.path.exists(export_folder):
 if not os.path.exists(export_folder):
     os.makedirs(export_folder)
 
-# Loop through the objects and export each one to a separate DAE file
-#for object_name in object_names:
-for obj in bpy.data.objects:
+for object_name in object_names:
     bpy.ops.object.select_all(action='DESELECT')
-    #obj = bpy.data.objects[object_name]
-    obj.select_set(True)
+    obj = bpy.data.objects[object_name]
     export_path = os.path.join(export_folder, obj.name + ".dae")
     bpy.ops.wm.collada_export(filepath=export_path, selected=True, apply_modifiers=True, open_sim=True, use_texture_copies=False)
 
+bpy.ops.object.select_all(action='SELECT')
+for object_name in object_names:
+    obj = bpy.data.objects[object_name]
+    obj.select_set(False)
+
+export_path = os.path.join(export_folder, os.path.splitext(os.path.basename(bpy.data.filepath))[0] + ".dae")
+bpy.ops.wm.collada_export(filepath=export_path, selected=True, apply_modifiers=True, open_sim=True, use_texture_copies=False)
