16b086e8fe58f9b789d4652a6733e13e3b910b74
Zai Dium <zai.dium@yahoo.com>
2023-07-15 21:56:40 +0300

    Minor Fixes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DieselPunkPlane
-Diesel Punk Plane
+
+The Mesh object Based on "Ryan Butler" project "Diesel Punk", with too many modifications I added, to use it in OpenSim
+
+https://sketchfab.com/3d-models/diesel-punk-plane-2f94582de15240d8b6a0ef3cc195f0dd
+
+Keep Credits for "Ryan Butler" and "Zai Dium" in Note Card inside the object
+
--- make_convex.py
+++ make_convex.py
@@ -1,4 +1,7 @@
-### Create mesh object of object that have a Face Map with name `Convex`
+"""
+    Create mesh object of objects that have a Face Map with name `Convex`,
+    it copy all faces in Convex face map into another new object, moving it to `Convex` collection
+"""
 import bpy
 import bmesh
 import os
@@ -17,48 +20,47 @@ else:
 
 if bpy.context.mode != "OBJECT":
     bpy.ops.object.mode_set(mode='OBJECT')
-    
+
 bpy.ops.object.select_all(action='DESELECT')
 
-## Collect objects that have facemap named "Convect"S
+## Collect objects that have facemap named "Convex"
 convexObjects = [obj for obj in bpy.data.objects if face_map_name in obj.face_maps]
-    
+
 for obj in convexObjects:
 
     if face_map_name in obj.face_maps:
-        
+
         bpy.context.view_layer.objects.active = obj
         obj.select_set(True)
-        
+
         mesh = obj.data
-        
+
         face_map_index = obj.face_maps[face_map_name].index
         face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
-        
+
         ## MUST BE in Object mode
-        face_map_index = obj.face_maps[face_map_name].index        
+        face_map_index = obj.face_maps[face_map_name].index
         face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
-        
+
         for i, fm_data in enumerate(face_map.data):
             # fm_data.value can be either -1 (unassigned) or the index of the face map it is assigned to
             selected = fm_data.value == face_map_index
             f = mesh.polygons[i]
-            f.select = selected # Select the face, maybe we do not need it        
-            
+            f.select = selected # Select the face, maybe we do not need it
+
         old_selected = [o for o in bpy.context.scene.objects]
-            
+
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1})
         bpy.ops.mesh.separate(type='SELECTED')
-        
+
         cur_selected = [o for o in bpy.context.scene.objects]
         new_obj = [o for o in cur_selected if o not in old_selected][0]
         print("org=", obj.name, "org=", new_obj.name)
 
         for coll in new_obj.users_collection:
             coll.objects.unlink(new_obj)
-            
+
         new_obj.name = obj.name+"_Convex"
         convex_collection.objects.link(new_obj)
         bpy.ops.object.mode_set(mode='OBJECT')
-        
