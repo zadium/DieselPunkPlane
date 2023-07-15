b86b29fbb3f7b4e2206de49adfdcf0cd293c810e
Zai Dium <zai.dium@yahoo.com>
2023-07-15 18:43:33 +0300

    Add new Propeller

--- create_convex.py
+++ create_convex.py
@@ -1,66 +1,105 @@
+### Create mesh object of object that have a Face Map with name `Convex`
 import bpy
 import bmesh
 import os
 from bl_ui.utils import PresetPanel
 from bpy.types import Panel, Menu
 
-#convex_names = ["Hull", "Wheels", "Wheel", "Wings"]
-convex_names = ["Hull"]
+convex_names = ["Hull", "Wings", "Wheels", "Wheel"]
 
-bpy.ops.object.mode_set(mode='OBJECT')
+face_map_name = "Convex"
 
-for object_name in convex_names:    
-    bpy.ops.object.select_all(action='DESELECT')
+#Create _Convex collection
+if "_Convex" in bpy.data.collections:
+    convex_collection = bpy.data.collections["_Convex"]
+else:
+    convex_collection = bpy.data.collections.new('_Convex')
+    bpy.context.scene.collection.children.link(convex_collection)
+
+if bpy.context.mode == "Object":
+    bpy.object.mode_set(mode='OBJECT')
+
+for object_name in convex_names:
+#for obj in bpy.data.objects:
+
+    #bpy.ops.object.select_all(action='DESELECT')
     obj = bpy.data.objects[object_name]
-    obj.select_set(True)
-    
-    obj = bpy.context.active_object
-    mesh = obj.data
-    
-# MUST BE in Object mode    
-    face_map_name = "Convex"
-#    face_map = obj.face_maps[face_map_name]
-    
-#    face_map = obj.face_maps.active        
-#    if face_map.name != face_map_name:
-#        face_map = obj.face_maps[face_map_name]
-#        bpy.ops.object.face_maps.active = face_map
-    face_map_index = obj.face_maps[face_map_name].index
+    if face_map_name in obj.face_maps:
 
-#    print("name="+mesh.face_maps[face_map_index].name);
-    # get its index
-    face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
+        #obj = bpy.context.active_object
+        mesh = obj.data
 
-    # run over the data of the face map data layer (only one layer, so [0])
-    for i, fm_data in enumerate(mesh.face_maps[0].data):
-        # select the polygon if its fm layer value correspond to the good face map
-        # fm_data.value can be either -1 (unassigned) or the index of the face map it is assigned to
-        mesh.polygons[i].select = fm_data.value == face_map_index
-    
-    bpy.ops.object.mode_set(mode='EDIT')
-    
-    bm = bmesh.from_edit_mesh(obj.data)
-    selected_faces = [f for f in bm.faces if f.select]
-    
-    bpy.ops.mesh.duplicate_move() #(give a warning because context is normally using the UI, but no consequences)
-    bpy.ops.mesh.separate(type='SELECTED')    
-    
-    bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1}, TRANSFORM_OT_translate={"value":(5, 0, 0), "orient_axis_ortho":'X', "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(True, False, False), "mirror":False, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":0.0630394, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_elements":{'INCREMENT'}, "use_snap_project":False, "snap_target":'CLOSEST', "use_snap_self":True, "use_snap_edit":True, "use_snap_nonedit":True, "use_snap_selectable":False, "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "view2d_edge_pan":False, "release_confirm":False, "use_accurate":False, "use_automerge_and_split":False})
+        face_map_index = obj.face_maps[face_map_name].index
+        face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
 
-    
-    bpy.ops.object.mode_set(mode='OBJECT')
+        obj.select_set(True)
+    #    if bpy.context.mode == "Object":
+    #        bpy.object.mode_set(mode='OBJECT')
 
-    # Create a new mesh from the selected faces
 
-    # Create a new object from the new mesh
-    new_obj = bpy.data.objects.new(obj.name+"_Convex", new_mesh)
-    bpy.context.scene.collection.objects.link(new_obj)
+    ## MUST BE in Object mode
+    #    face_map = obj.face_maps[face_map_name]
 
-    # Select the new object
-    bpy.context.view_layer.objects.active = new_obj
-        
-#    bpy.ops.mesh.select_mode(type='FACE')
-#    bpy.ops.mesh.select_all(action='DESELECT')
+    #    face_map = obj.face_maps.active
+    #    if face_map.name != face_map_name:
+    #    face_map = obj.face_maps[face_map_name]
+    #    bpy.ops.object.face_maps.active = face_map
+        face_map_index = obj.face_maps[face_map_name].index
 
-            
-#    bpy.ops.object.face_map_select()
+    #    print("name="+mesh.face_maps[face_map_index].name);
+
+        face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
+
+        selected_faces = []
+
+        for i, fm_data in enumerate(face_map.data):
+            # fm_data.value can be either -1 (unassigned) or the index of the face map it is assigned to
+            selected = fm_data.value == face_map_index
+            f = mesh.polygons[i]
+            f.select = selected # Select the face, maybe we do not need it
+            if selected:
+                selected_faces.append(f)
+
+        #bpy.context.mode_set(mode='EDIT')
+        #bpy.object.mode_set(mode='OBJECT')
+        #bm = bmesh.from_edit_mesh(obj.data)
+        #selected_faces = [f for f in bm.faces if f.select]
+
+        #bpy.ops.object.mode_set(mode='OBJECT')
+        #selected_faces = [f for f in mesh.polygons if f.select]
+        #mesh.validate()
+        vertices = []
+        for f in selected_faces:
+            for v in f.vertices:
+                vertices.append(mesh.vertices[v].co)
+
+        faces = []
+        for f in selected_faces:
+            faces.append(f.vertices)
+
+        #new_mesh = bpy.data.meshes.new_from_object(obj)
+        new_mesh = bpy.data.meshes.new(obj.name+"_Convex_Mesh")
+        #new_mesh.from_pydata([v.co for f in selected_faces for v in f.vertices], [], [[v.index for v in f.vertices] for f in selected_faces])
+        new_mesh.from_pydata(vertices, [], faces)
+        mesh.update(calc_edges=True)
+
+        # Create a new object from the new mesh
+        new_obj = bpy.data.objects.new(obj.name+"_Convex", new_mesh)
+
+        convex_collection.objects.link(new_obj)
+        #bpy.context.scene.collection.objects.link(new_obj)
+
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(False)
+        new_obj.select_set(True)
+
+        for mod in obj.modifiers:
+            if mod.type == 'MIRROR':
+                bpy.ops.object.modifier_copy_to_selected(modifier=mod.name)
+                new_obj.modifiers[-1].name = mod.name+"_Convex"
+
+        #bpy.ops.object.move_to_collection()
+
+    #    bpy.ops.mesh.select_mode(type='FACE')
+    #    bpy.ops.mesh.select_all(action='DESELECT')
+    #    bpy.ops.object.face_map_select()
--- make_convexy.py
+++ make_convexy.py
@@ -5,33 +5,31 @@ import os
 from bl_ui.utils import PresetPanel
 from bpy.types import Panel, Menu
 
-#convex_names = ["Hull", "Wings", "Wheels", "Wheel"]
-convex_names = ["Hull"]
-
 face_map_name = "Convex"
+collection_convex_name = "Convex"
 
 #Create _Convex collection
-if "_Convex" in bpy.data.collections:
-    convex_collection = bpy.data.collections["_Convex"]
+if collection_convex_name in bpy.data.collections:
+    convex_collection = bpy.data.collections[collection_convex_name]
 else:
-    convex_collection = bpy.data.collections.new('_Convex')
+    convex_collection = bpy.data.collections.new(collection_convex_name)
     bpy.context.scene.collection.children.link(convex_collection)
 
 if bpy.context.mode != "OBJECT":
     bpy.ops.object.mode_set(mode='OBJECT')
-
-for object_name in convex_names:
-#for obj in bpy.data.objects:
-
-    #bpy.ops.object.select_all(action='DESELECT')
-    #bpy.context.view_layer.objects.active = obj
-    obj = bpy.data.objects[object_name]
     
+bpy.ops.object.select_all(action='DESELECT')
+
+## Collect objects that have facemap named "Convect"S
+convexObjects = [obj for obj in bpy.data.objects if face_map_name in obj.face_maps]
     
+for obj in convexObjects:
+
     if face_map_name in obj.face_maps:
         
-        obj.select_set(True)
         bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+        
         mesh = obj.data
         
         face_map_index = obj.face_maps[face_map_name].index
@@ -47,15 +45,20 @@ for object_name in convex_names:
             f = mesh.polygons[i]
             f.select = selected # Select the face, maybe we do not need it        
             
+        old_selected = [o for o in bpy.context.scene.objects]
+            
         bpy.ops.object.mode_set(mode='EDIT')
-
         bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1})
         bpy.ops.mesh.separate(type='SELECTED')
+        
+        cur_selected = [o for o in bpy.context.scene.objects]
+        new_obj = [o for o in cur_selected if o not in old_selected][0]
+        print("org=", obj.name, "org=", new_obj.name)
 
-        bpy.ops.object.mode_set(mode='OBJECT')        
-        new_objs = [obj for obj in bpy.context.selected_objects if obj != bpy.context.object]
-        new_obj = new_objs[0]
-
+        for coll in new_obj.users_collection:
+            coll.objects.unlink(new_obj)
+            
         new_obj.name = obj.name+"_Convex"
-        #bpy.context.scene.collection.objects.unlink(new_obj)            
-        convex_collection.objects.link(new_obj)        
+        convex_collection.objects.link(new_obj)
+        bpy.ops.object.mode_set(mode='OBJECT')
+        
