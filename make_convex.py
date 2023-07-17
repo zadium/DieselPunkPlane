e6706f2e8d3da3121cf28e895288394c83ab149e
Zai Dium <zai.dium@yahoo.com>
2023-07-17 15:57:30 +0300

    Fix Export all to be functions, and export in one file

--- make_convex.py
+++ make_convex.py
@@ -10,63 +10,66 @@ import os
 from bl_ui.utils import PresetPanel
 from bpy.types import Panel, Menu
 
-face_map_name = "Convex"
-collection_convex_name = "Convex"
-name_suffex = "-Convex"
+def create_convex():
+    face_map_name = "Convex"
+    collection_convex_name = "Convex"
+    name_suffex = "-Convex"
 
-#Create _Convex collection
-if collection_convex_name in bpy.data.collections:
-    convex_collection = bpy.data.collections[collection_convex_name]
-else:
-    convex_collection = bpy.data.collections.new(collection_convex_name)
-    bpy.context.scene.collection.children.link(convex_collection)
+    #Create _Convex collection
+    if collection_convex_name in bpy.data.collections:
+        convex_collection = bpy.data.collections[collection_convex_name]
+    else:
+        convex_collection = bpy.data.collections.new(collection_convex_name)
+        bpy.context.scene.collection.children.link(convex_collection)
 
-if bpy.context.mode != "OBJECT":
-    bpy.ops.object.mode_set(mode='OBJECT')
+    if bpy.context.mode != "OBJECT":
+        bpy.ops.object.mode_set(mode='OBJECT')
 
-bpy.ops.object.select_all(action='DESELECT')
+    bpy.ops.object.select_all(action='DESELECT')
 
-## Collect objects that have facemap named "Convex"
-convexObjects = [obj for obj in bpy.data.objects if (not obj.name.endswith(name_suffex)) and face_map_name in obj.face_maps]
+    ## Collect objects that have facemap named "Convex"
+    convexObjects = [obj for obj in bpy.data.objects if (not obj.name.endswith(name_suffex)) and face_map_name in obj.face_maps]
 
-for obj in convexObjects:
-    
-    new_name = obj.name + name_suffex
-    if new_name in bpy.data.objects:
-        new_obj = bpy.data.objects[new_name]
-        bpy.data.objects.remove(new_obj, do_unlink=True)
+    for obj in convexObjects:
         
-    bpy.context.view_layer.objects.active = obj
-    obj.select_set(True)
+        new_name = obj.name + name_suffex
+        if new_name in bpy.data.objects:
+            new_obj = bpy.data.objects[new_name]
+            bpy.data.objects.remove(new_obj, do_unlink=True)
+            
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
 
-    mesh = obj.data
+        mesh = obj.data
 
-    face_map_index = obj.face_maps[face_map_name].index
-    face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
+        face_map_index = obj.face_maps[face_map_name].index
+        face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
 
-    ## MUST BE in Object mode
-    face_map_index = obj.face_maps[face_map_name].index
-    face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
+        ## MUST BE in Object mode
+        face_map_index = obj.face_maps[face_map_name].index
+        face_map = mesh.face_maps[face_map_index] # no name for face map inside mesh
 
-    for i, fm_data in enumerate(face_map.data):
-        # fm_data.value can be either -1 (unassigned) or the index of the face map it is assigned to
-        selected = fm_data.value == face_map_index
-        f = mesh.polygons[i]
-        f.select = selected # Select the face, maybe we do not need it
+        for i, fm_data in enumerate(face_map.data):
+            # fm_data.value can be either -1 (unassigned) or the index of the face map it is assigned to
+            selected = fm_data.value == face_map_index
+            f = mesh.polygons[i]
+            f.select = selected # Select the face, maybe we do not need it
 
-    old_selected = [o for o in bpy.context.scene.objects]
+        old_selected = [o for o in bpy.context.scene.objects]
 
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1})
-    bpy.ops.mesh.separate(type='SELECTED')
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.duplicate_move(MESH_OT_duplicate={"mode":1})
+        bpy.ops.mesh.separate(type='SELECTED')
 
-    cur_selected = [o for o in bpy.context.scene.objects]
-    new_obj = [o for o in cur_selected if o not in old_selected][0]
+        cur_selected = [o for o in bpy.context.scene.objects]
+        new_obj = [o for o in cur_selected if o not in old_selected][0]
 
-    for coll in new_obj.users_collection:
-        coll.objects.unlink(new_obj)
+        for coll in new_obj.users_collection:
+            coll.objects.unlink(new_obj)
 
-    new_obj.name = new_name
-    new_obj.data.name = new_obj.name
-    convex_collection.objects.link(new_obj)
-    bpy.ops.object.mode_set(mode='OBJECT')
+        new_obj.name = new_name
+        new_obj.data.name = new_obj.name
+        convex_collection.objects.link(new_obj)
+        bpy.ops.object.mode_set(mode='OBJECT')
+        
+create_convex()        
